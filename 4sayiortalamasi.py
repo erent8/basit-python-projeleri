9a48d7baad8ff2ea117413abf664f905e2b577aa
Eren <86615310+erent8@users.noreply.github.com>
2022-11-20 00:20:36 +0300

    Create 4sayiortalamasi.py

---
+++ 4sayiortalamasi.py
@@ -0,0 +1,7 @@
+sayi1 = input('1. Sayı : ')
+sayi2 = input('2. Sayı : ')
+sayi3 = input('3. Sayı : ')
+sayi4 = input('4. Sayı : ')
+ortalama=(int(sayi1)+int(sayi2)+int(sayi3)+int(sayi4))/4
+print("Ortalama :{0} ".format(ortalama))
+
