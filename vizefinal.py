96f8690cf8468bbc1f27e751048d0eeaa66dbccb
Eren <86615310+erent8@users.noreply.github.com>
2022-11-20 00:29:54 +0300

    Create vizefinal.py

---
+++ vizefinal.py
@@ -0,0 +1,6 @@
+# Ahi Evran Üniversitesi Vize Final Notu Hesaplama
+
+vize = input('Vize Notunuz: ')
+final = input('Final Notunuz: ')
+ortalama = (float(vize)*0.4) + (float(final)*0.6)
+print("Ortalama: {0}" .format(ortalama))
