cb31f94acdce2f8ffcad35530ed8eabf1e00c63b
Eren <86615310+erent8@users.noreply.github.com>
2022-11-20 00:53:55 +0300

    Create yaziliortalamahesaplama.py

---
+++ yaziliortalamahesaplama.py
@@ -0,0 +1,6 @@
+y1 = input('1. yazılı : ')
+y2 = input('2. yazılı : ')
+y3 = input('3. yazılı : ')
+y4 = input('4. yazılı : ')
+ortalama = (float(y1)+float(y2)+float(y3)+float(y4))/4
+print("ortalama:{0}" .format(ortalama))
